ceab21f0d6b7f913a2e1ba3ab9b77e282bf448ef
DaneelD <106441315+DaneelD@users.noreply.github.com>
2022-11-26 12:06:33 -0500

    Update General_Employee.java
    Add in attributes, constructor, accessor and mutators functions

--- General_Employee.java
+++ General_Employee.java
@@ -1,5 +1,60 @@
 //package sbs_app;
-public class General_Employee {
+public class General_Employee extends Person {
+
+    private employeeType empType;
+
+    //Constructor
+    public General_Employee(String name, String dob, Sex sex, String addr, String email, String telNum, employeeType empType) {
+        super(name, dob, sex, addr, email, telNum);
+        this.empType = empType;
+    }
+
+    //Acessor Functions
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDOB() {
+        return dob;
+    }
+
+    @Override
+    public Sex getSex(){ 
+        return sex;
+    }
+
+    @Override
+    public String getAddr(){
+        return address;
+    }
+
+    @Override
+    public String getEmail(){ 
+        return email;
+    }
+
+    @Override
+    public String getTelNum() {
+        return telNum;
+    }
+
+    //Mutators Function
+    @Override
+    public void setAddr(String newAddr) {
+        address = newAddr;
+        
+    }
+
+    @Override
+    public void setEmail(String new_Email) {
+        email = new_Email;  
+    }
+
+    @Override
+    public void setTelNum(String Tel_num) {
+        telNum = Tel_num;
+    }
     
 }
-
